a9c1c183319e3e734b3e08e91bf4d75d91325873
Howie Zuo <etsushi.sa@gmail.com>
2015-12-28 11:36:14 +0900

    configure Kotlin

---
+++ app/src/androidTest/java/howiezuo/github/io/androidkotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package howiezuo.github.io.androidkotlin
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
